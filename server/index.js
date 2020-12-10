92e10c0e0fc2ef454b964e3c5a51d8f0b0e69132
aoki1224 <41281347+aoki1224@users.noreply.github.com>
2020-12-11 03:07:34 +0900

    デプロイ用設定

--- server/index.js
+++ server/index.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 const productRoutes = require('./route/products');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || '3001';
+const appPath = path.join(__dirname, '..', 'dist', 'reservation-app')
+
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(
   () =>
   {
-    const fakeDb = new FakeDb();
-    fakeDb.initDb;
+    if (process.env.NODE_ENV !== 'production')
+    {
+      const fakeDb = new FakeDb();
+      // fakeDb.initDb;
+    }
+
   }
 );
+
 app.use('/api/v1/products', productRoutes);
+
 app.listen(PORT, function ()
 {
   console.log('I am running')
 });
+
+app.use(express.static(appPath))
+
+if (process.env.NODE_ENV === 'production')
+{
+  app.get("#", function (req, res)
+  {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
 
