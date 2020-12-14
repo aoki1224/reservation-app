d1e7cd94ac9cf4037af939a82e4134c9ed06ffbd
aoki1224 <41281347+aoki1224@users.noreply.github.com>
2020-12-14 10:24:18 +0900

    PORT修正

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 const app = express();
 // const PORT = process.env.PORT || '3001';
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || '3001';
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
